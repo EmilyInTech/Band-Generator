7acd739040daf83fff647f16cf8d2d7f91c4e5ad
Emily Hydlebeerg <143048126+EmilyInTech@users.noreply.github.com>
2023-12-19 02:31:19 -0500

    Add files via upload

---
+++ Band generater.py
@@ -0,0 +1,4 @@
+print("Welcome to the big band generater.")
+City = input("What city did you grow up in?\n")
+Pet = input("What is the name of your pet?\n")
+print("Your band name is " + City + " " + Pet)
